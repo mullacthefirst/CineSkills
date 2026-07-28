b834e89a149bddd61b151b8cc618451938fbc126
mullacthefirst <callumoconnor2012@gmail.com>
2026-07-28 23:45:13 +0100

    Renamed application from CineGrade to CineSkills

--- app.js
+++
@@ -1,4 +0,0 @@
-// CineGrade Application Entry Point Redirect
-// The monolithic app.js has been refactored into ES6 Modules located under the js/ directory:
-// js/state.js, js/matrix.js, js/profile.js, js/charts.js, js/gear.js, js/quests.js, js/app.js
-import './js/app.js';
